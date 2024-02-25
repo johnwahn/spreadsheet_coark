7fa05d056e36ef7637209d0c3121d04422761581
John Ahn <johnahn@Johns-MacBook-Pro.local>
2024-02-25 12:06:09 -0700

    Create pressable components to spreadsheet in the Start page

--- App/screens/Start.tsx
+++ App/screens/Start.tsx
@@ -4,34 +4,64 @@ import { useNavigation } from '@react-navigation/native'
 
 interface NavigationParams {
    numberOfResturants: string;
+   tableName: string;
 }
 
 const Start = () => {
-    const [inputRow, setInputRow] = useState('')
+    const [numberInput, setNumberInput] = useState('');
+    const [nameInput, setNameInput] = useState('');
+    const [paramList, setParamList] = useState([]);
     const navigation = useNavigation();
     
-    const params: NavigationParams = {
-      numberOfResturants: inputRow,
-    };
+   /* const params: NavigationParams = {
+      numberOfResturants: numberInput,
+      tableNames: tableNameInputs,
+    };*/
 
-    const createSpreadSheet = () => {
-       navigation.navigate('Spreadsheet', params);
+    const updateTableListContent = () => {
+      const params: NavigationParams = {
+         numberOfResturants: numberInput,
+         tableName: nameInput,
+       };
+      setParamList([...paramList, params]);
     }
+
+    const navigateToSpreadSheet = (params) => {
+      navigation.navigate('Spreadsheet', params);
+    }
+
+
     return(
  <View style={styles.container}>
     <Text>Welcome!.</Text>
     <View style={styles.inputContainer}>
+      <Text>Name : </Text>
+      <TextInput 
+            style={{width: 200, borderWidth: 2, marginRight: 10}} 
+            placeholder='Enter Name'
+            onChangeText={text => setNameInput(text)}
+            value={nameInput}
+            />
         <Text>How Many Resturants?: </Text>
         <TextInput 
             style={{width: 200, borderWidth: 2, marginRight: 10}} 
             placeholder='Enter number'
-            onChangeText={text => setInputRow(text)}
-            value={inputRow}/>
+            onChangeText={text => setNumberInput(text)}
+            value={numberInput}/>
     </View>
-    <Pressable role = "button" style = {{borderColor: 'red'}} onPress={createSpreadSheet}>
+    <Pressable role="button" style={{borderColor: 'red'}} onPress={updateTableListContent}>
         <Text>Create Table</Text>
     </Pressable>
-
+      {/* Display Table Names as Pressable Links */}
+      {paramList.length > 0 && (
+      <View>
+         {paramList.map((params, index) => (
+         <Pressable role = "button" onPress={() => navigateToSpreadSheet(params)}  key={index}>
+            <Text>{params.tableName}</Text>
+         </Pressable>
+         ))}
+      </View>
+      )}
  </View>
  );
 };
